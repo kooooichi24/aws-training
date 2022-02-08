4cf41215c0477f93cc42b4c31c90014787b8ffc9
kooooichi24 <ex.kouichi1224@gmail.com>
2022-02-09 01:06:42 +0900

    GET /api/v1/todo を作成

--- ch2/05/sls-rest-api/handler.ts
+++ ch2/05/sls-rest-api/handler.ts
@@ -3,17 +3,38 @@ import {
   DynamoDBClient,
   PutItemCommand,
   PutItemCommandInput,
+  ScanCommand,
+  ScanCommandInput,
+  ScanCommandOutput,
 } from "@aws-sdk/client-dynamodb";
 import { uuid } from "uuidv4";
 import dayjs from "dayjs";
+
+const client = new DynamoDBClient({ region: "ap-northeast-1" });
+
+export async function getAllTodo(
+  event: Lambda.APIGatewayEvent
+): Promise<Lambda.APIGatewayProxyResult> {
+  const params: ScanCommandInput = {
+    TableName: process.env.DYNAMODB_TODO_TABLE,
+  };
+
+  const command = new ScanCommand(params);
+  const response: ScanCommandOutput = await client.send(command);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      todos: response.Items,
+    }),
+  };
+}
 
 export async function createTodo(
   event: Lambda.APIGatewayEvent
 ): Promise<Lambda.APIGatewayProxyResult> {
   const data = JSON.parse(event.body);
-  const client = new DynamoDBClient({ region: "ap-northeast-1" });
   const params: PutItemCommandInput = {
-    /** input parameters */
     TableName: process.env.DYNAMODB_TODO_TABLE,
     Item: {
       id: {
@@ -36,32 +57,15 @@ export async function createTodo(
       },
     },
   };
-  const command = new PutItemCommand(params);
 
-  try {
-    await client.send(command);
-    return {
-      statusCode: 200,
-      body: JSON.stringify(
-        {
-          message: "Go Serverless v1.0! Your function executed successfully!",
-          input: event,
-        },
-        null,
-        2
-      ),
-    };
-  } catch (e) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({
-        message: "error",
-      }),
-      headers: {
-        "content-type": "application/json",
-      },
-    };
-  } finally {
-    client.destroy();
-  }
+  const command = new PutItemCommand(params);
+  await client.send(command);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Go Serverless v1.0! Your function executed successfully!",
+      input: event,
+    }),
+  };
 }
